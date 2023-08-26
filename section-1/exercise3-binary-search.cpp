d80cf8b9d146f12bafcb1e12334bfef28f7dd110
Júlio Juriolli <juliocesarjuriolli@gmail.com>
2023-08-26 18:47:19 -0300

    fix: some changes

--- section-1/exercise3-binary-search.cpp
+++ section-1/exercise3-binary-search.cpp
@@ -1,111 +1,106 @@
 /*
-* 1) Crie uma funcao que receba o nuemro de
-* matricula de um aluno e retorne suas informacoes
-* usando a busca binaria. E possivel criar uma funcao de
-* busca binaria que receba a matricula de um aluno e retorne suas informacoes?
-* Caso negativo, quais seriam as alternativas? Justifique sua resposta.
-* 
-* Sim é possivel criar. A resposta esta no codigo abaixo. Nele, conseguimos fazer essa busca
-* Porem deve estar em ordem crescente as matriculas
-*
-* 2) Crie uma funcao que receba o nome de um aluno e retorne suas informacoes.
-* E possivel criar uma funcao de busca binaria que receba o nome de um aluno e
-* retorne suas informacoes? Justifique sua resposta. Caso negativo, quais seriam
-* as alternativas? Justifique sua resposta.
-*
-* Não há como pois não é possível ordenar os nomes em ordem crescente
-* Logo a busca resultara em falhas
-* 
-* 3) Crie uma funcao que receba um valor inteiro indicando uma idade e retorne a
-* quantidade de alunos que tem essa idade ou menos. E possivel criar uma funcao de
-* busca binaria que receba a idade de um aluno e retorne a  soma pedida? Caso negativo,
-* quais seriam as alternativas? Justifique sua resposta.
-*
-* Nao e possivel pois nao conseguimos percorrer a todos os estudantes utilizando a busca binaria
-*/
+ * 1) Create a function that receives a student's registration number and returns their information
+ * using binary search. Is it possible to create a binary search function that receives a student's
+ * registration number and returns their information? If not, what would be the alternatives?
+ * Justify your answer.
+ *
+ * Yes, it is possible to create. The answer is in the code below. In it, we can perform this search,
+ * but the registration numbers must be in ascending order.
+ *
+ * 2) Create a function that receives a student's name and returns their information.
+ * Is it possible to create a binary search function that receives a student's name and
+ * returns their information? Justify your answer. If not, what would be the alternatives?
+ * Justify your answer.
+ *
+ * It is not possible because it is not feasible to sort names in ascending order.
+ * Therefore, the search would result in failures.
+ *
+ * 3) Create a function that receives an integer value indicating an age and returns the
+ * quantity of students who are that age or younger. Is it possible to create a binary
+ * search function that receives a student's age and returns the requested sum? If not,
+ * what would be the alternatives? Justify your answer.
+ *
+ * It's not possible because we cannot traverse all the students using binary search.
+ */
 
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-// Definio da estrutura 'student' que armazena informacoes de um aluno
+// Definition of the 'student' structure that stores information about a student
 struct student
 {
-    int matricula;
-    string nome;
-    int idade;
+    int id;
+    string name;
+    int age;
 };
 
-// Funcao para criar um novo aluno com informacoes inseridas pelo usuario
+// Function to create a new student with information entered by the user
 student createStudent(int i)
 {
-    cout << "Criacao do aluno " << i << endl;
+    cout << "Creating student " << i << endl;
     student newStudent;
-    cout << "Insira a Matricula do aluno: " << endl;
-    cin >> newStudent.matricula;
-    cout << "Insira o nome do aluno: " << endl;
-    cin >> newStudent.nome;
-    cout << "Insira a idade do aluno: " << endl;
-    cin >> newStudent.idade;
+    cout << "Insert student's ID: " << endl;
+    cin >> newStudent.id;
+    cout << "Insert student's name: " << endl;
+    cin >> newStudent.name;
+    cout << "Insert student's age: " << endl;
+    cin >> newStudent.age;
     return newStudent;
 }
 
-// Funcao para imprimir os dados de um aluno
+// Function to print student data
 void printStudentData(student students[], int index)
 {
-    cout << "Matricula: " << students[index].matricula << endl;
-    cout << "Nome: " << students[index].nome << endl;
-    cout << "Idade: " << students[index].idade << endl;
+    cout << "Id: " << students[index].id << endl;
+    cout << "Name: " << students[index].name << endl;
+    cout << "Age: " << students[index].age << endl;
 }
 
-// Funcao de busca binaria por Matricula de aluno
-int binaryFindStudentByMatricula(student students[], int numberOfStudents, int matricula)
+// Binary search for students id
+int binaryFindStudentById(student students[], int numberOfStudents, int id)
 {
-    int firstIndex = 0;
-    int lastIndex = numberOfStudents - 1;
-    int halfIndex;
+    int arrayBeginningIndex = 0;
+    int arrayFinishingIndex = numberOfStudents - 1;
+    int arrayMiddleIndex;
     int executions = 0;
 
-    halfIndex = (firstIndex + lastIndex) / 2;
-    // Loop de busca binaria
-    
-    do {
-        
-        halfIndex = (firstIndex + lastIndex) / 2; // Atualiza o indice do meio
+    arrayMiddleIndex = (arrayBeginningIndex + arrayFinishingIndex) / 2;
+    // Binary search loop
 
-        // Verifica se a Matricula do aluno no indice do meio  igual aa Matricula procurado
-        if (students[halfIndex].matricula == matricula)
+    do
+    {
+        arrayMiddleIndex = (arrayBeginningIndex + arrayFinishingIndex) / 2; // Update the middle index
+
+        // Check if student's ID at the middle index is equal to the searched ID
+        if (students[arrayMiddleIndex].id == id)
         {
-            cout << "Finalizada com " << executions << " execucoes" << endl;
-            return halfIndex; // Retorna o indice onde o aluno foi encontrado
+            cout << "Search ended after " << executions << " executions" << endl;
+            return arrayMiddleIndex; // Return the index where the student was found
         }
         else
         {
-            // Atualiza os  indices do primeiro e ultimo elementos da faixa de busca
-            if (students[halfIndex].matricula < matricula)
-            {
-                firstIndex = halfIndex + 1;
-            }
-            else if (students[halfIndex].matricula > matricula)
-            {
-                lastIndex = halfIndex - 1;
-            }
+            // Update the indexes of the first and last elements in the search range
+            if (students[arrayMiddleIndex].id < id)
+                arrayBeginningIndex = arrayMiddleIndex + 1;
+            else if (students[arrayMiddleIndex].id > id)
+                arrayFinishingIndex = arrayMiddleIndex - 1;
         }
-        executions++; // Incrementa o contador de execucoes
-    } while (firstIndex <= lastIndex);
+        executions++; // Increment the execution counter
+    } while (arrayBeginningIndex <= arrayFinishingIndex);
 
-    // Exibe o numero de execucoes e retorna -1 para indicar que o aluno no foi encontrado
-    cout << "Finalizada com " << executions << " execucoes" << endl;
+    // Display the number of executions and return -1 to indicate that the student was not found
+    cout << "Search ended after " << executions << " executions" << endl;
     return -1;
 }
 
 int main()
 {
-    int matriculaToFindBinary = 5; // Matricula do aluno a ser procurado usando busca binaria
+    int matriculaToFindBinary = 5; // Id student's a ser procurado usando binary search
 
     int numberOfStudents;
-    cout << "Insira o numero de alunos: " << endl;
+    cout << "Insert Student's numero de alunos: " << endl;
     cin >> numberOfStudents; // Insere o numero de alunos
 
     student students[numberOfStudents]; // Criacao de um array de alunos
@@ -114,18 +109,14 @@ int main()
     for (int i = 0; i < numberOfStudents; i++)
         students[i] = createStudent(i);
 
-    // Realiza a busca binaria pelo aluno com a Matricula especificado
-    int queryResult = binaryFindStudentByMatricula(students, numberOfStudents, matriculaToFindBinary);
+    // Realiza a binary search pelo aluno com a Id especificado
+    int queryResult = binaryFindStudentById(students, numberOfStudents, matriculaToFindBinary);
 
-    // Verifica se o resultado da busca foi bem-sucedido ou no
+    // Verify if o resultado da search was bem-sucedido ou no
     if (queryResult != -1)
-    {
-        printStudentData(students, queryResult); // Imprime os dados do aluno encontrado
-    }
+        printStudentData(students, queryResult); // Imprime os dados student's encontrado
     else
-    {
-        cout << "Busca binaria pela Matricula " << matriculaToFindBinary << " falhou" << endl;
-    }
+        cout << "Busca binaria pela Id " << matriculaToFindBinary << " falhou" << endl;
 
     return 0;
 }
